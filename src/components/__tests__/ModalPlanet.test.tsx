434cb2e71ebee2d7f4c9f30e80363a8e770875fe
Emeline <emeline.courcelle@gmail.com>
2024-09-12 11:33:58 +0200

    New test on ModalPlanet

--- src/components/__tests__/ModalPlanet.test.tsx
+++ src/components/__tests__/ModalPlanet.test.tsx
@@ -3,20 +3,48 @@ import { useGetPlanet } from '../../hooks/useGetPlanet';
 import { render, screen } from '@testing-library/react';
 import { vi, describe, it, expect  } from 'vitest';
 import { useNavigate, useParams } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 vi.mock('../../hooks/useGetPlanet', () => ({
     useGetPlanet: vi.fn(),
   }));
 
-vi.mock('react-router-dom', () => ({
-   useNavigate: vi.fn(),
-   useParams: vi.fn(),
-}));
+vi.mock("react-router-dom", async (importOriginal) => {
+  const actual = await importOriginal()
+  return {
+    ...actual,
+    useNavigate: vi.fn(),
+    useParams: vi.fn().mockReturnValue({ planetId: '1' }),
+  }
+})
 
 describe("ModalPlanet Component", () => {
     const mockUseGetPlanet = useGetPlanet as ReturnType<typeof vi.fn>;
     const mockUseNavigate = useNavigate as ReturnType<typeof vi.fn>;
     const mockUseParams = useParams as ReturnType<typeof vi.fn>;
+
+    const renderComponent = (planetId: string) =>
+      render(
+        <MemoryRouter initialEntries={[`/planets/${planetId}`]}>
+            <Routes>
+                <Route path="/planets/:planetId" element={<ModalPlanet />} />
+            </Routes>
+        </MemoryRouter>
+      );
+
+    it('shows skeleton when data is pending', () => {
+        (useGetPlanet as vi.Mock).mockReturnValue({
+            planet: null,
+            isPending: true,
+            error: null,
+        });
+
+        renderComponent('1');
+
+        expect(screen.getAllByTestId('skeleton')).toHaveLength(5);
+    });
+
+    
 
     it("should display planet details when data is fetched successfully", () => {
         mockUseGetPlanet.mockReturnValue({
@@ -35,7 +63,7 @@ describe("ModalPlanet Component", () => {
 
           mockUseParams.mockReturnValue({ planetId: '1' });
 
-        render(<ModalPlanet />);
+        renderComponent('1');
 
         expect(screen.getByText('Planet:')).toBeInTheDocument();
         expect(screen.getByText('Tatooine')).toBeInTheDocument();
